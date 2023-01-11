5a0c206a2df244abe1434a5c2322543b7a8e072f
nicholas <nicholaschuawork@gmail.com>
2023-01-11 12:38:00 +0800

    Updated the env to include only 1 version

--- lib/neucore/env.py
+++ lib/neucore/env.py
@@ -1,4 +1,4 @@
-url="https://api.ailiverse.com"
+url="http://35.197.135.77:8085"
 
 SIGN_UP_URL="{}/signUp".format(url)
 SIGN_IN_URL="{}/signIn".format(url)
@@ -10,15 +10,6 @@ INFER_URL="{}/infer".format(url)
 INFER_ASYNC_URL="{}/inferAsync".format(url)
 RESULTS_URL="{}/results".format(url)
 
-url_2 = "http://34.80.13.82:8085"
-
-SIGN_UP_URL_2="{}/signUp".format(url_2)
-SIGN_IN_URL_2="{}/signIn".format(url_2)
-CREATE_MODEL_URL_2="{}/createModel".format(url_2)
-INFER_URL_2="{}/infer".format(url_2)
-INFER_ASYNC_URL_2="{}/inferAsync".format(url_2)
-RESULTS_URL_2="{}/results".format(url_2)
-
 URL_DICT = {
     "1" : { "SIGN_UP_URL" : SIGN_UP_URL, # version 1
             "SIGN_IN_URL" : SIGN_IN_URL,
@@ -29,12 +20,4 @@ URL_DICT = {
             "INFER_URL": INFER_URL,
             "INFER_ASYNC_URL": INFER_ASYNC_URL,
             "RESULTS_URL": RESULTS_URL},
-    "2" : {
-        "SIGN_UP_URL": SIGN_UP_URL_2, # version 2
-        "SIGN_IN_URL": SIGN_IN_URL_2,
-        "CREATE_MODEL_URL": CREATE_MODEL_URL_2,
-        "INFER_URL": INFER_URL_2,
-        "INFER_ASYNC_URL": INFER_ASYNC_URL_2,
-        "RESULTS_URL": RESULTS_URL_2
-    }
 }
