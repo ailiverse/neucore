80ba9aed9bc5edf712fe5f29e03f37052c17e4db
nicholas <nicholaschuawork@gmail.com>
2023-01-11 21:05:06 +0800

    Change to infer_load and createModel_load

--- lib/neucore/env.py
+++ lib/neucore/env.py
@@ -2,12 +2,12 @@ url="http://35.197.135.77:8085"
 
 SIGN_UP_URL="{}/signUp".format(url)
 SIGN_IN_URL="{}/signIn".format(url)
-CREATE_MODEL_URL="{}/createModel".format(url)
+CREATE_MODEL_URL="{}/createModel_load".format(url)
 UPLOAD_URL="{}/userUpload".format(url)
 STATUS_URL="{}/status".format(url)
 TRAIN_URL="{}/train".format(url)
-INFER_URL="{}/infer".format(url)
-INFER_ASYNC_URL="{}/inferAsync".format(url)
+INFER_URL="{}/infer_load".format(url)
+INFER_ASYNC_URL="{}/inferAsync_load".format(url)
 RESULTS_URL="{}/results".format(url)
 
 URL_DICT = {
