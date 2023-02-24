afa69faa0a35e1160e8cb58afc15252e0373f554
nicholas <nicholaschuawork@gmail.com>
2023-02-24 09:22:24 +0800

    Updated the version to 1.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 from distutils.core import setup
 
 setup(name='neucore',
-      version='1.0.5',
+      version='1.0.6',
       description='Ailiverse API Package',
       author='Nicholas Chua, Burhan Ul Tayyab',
       author_email='nicholas@ailiverse.com, burhan@ailiverse.com',
